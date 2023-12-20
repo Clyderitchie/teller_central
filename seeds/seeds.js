247f5285429539349b04ef05995986d608e18b1c
francis <clyderitchie@yahoo.com>
2023-12-19 19:23:02 -0800

    Restarting the models

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,22 +1,20 @@
 const sequelize = require('../config/connection');
-const { Client, Account, AccountInfo, Teller } = require('../models');
+const { Client, Account, Teller } = require('../models');
 
 const personalData = require('./personalData.json');
 const accountData = require('./accountData.json');
-const accountInfoData = require('./accountInfoData.json');
 const tellerData = require('./tellerData.json');
 
 const seedDatabase = async () => {
 
     await sequelize.sync({ force: true });
     
-    const account = await AccountInfo.bulkCreate(accountInfoData);
+    const account = await Account.bulkCreate(accountData);
 
     const personalClient = await Client.bulkCreate(personalData);
 
-    const accountInfo = await Account.bulkCreate(accountData);
-
     const tellerInfo = await Teller.bulkCreate(tellerData);
+    
     process.exit(0);
 };
 
